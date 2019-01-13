13bf4faa3a6d913604a331bab9895b8271e89123
Heather Kirk <hskirk@gmail.com>
2019-01-13 07:15:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+
+def line 
+  
+  puts "The line is currently empty."
+end
+
+def take_a_number(array,string)
+  katz_deli = []
+  string = "Welcome #{name}. You are number #{number} in line."
+end
+
+def now_serving
+  count = 1
+  puts "There is nobody waiting to be served."
+end
